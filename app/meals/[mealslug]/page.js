c9844305c44596fd7dbf5c6f276d67df293ee727
Eslam AbdelWahab <mohaeslam22@gmail.com>
2025-10-04 12:21:13 +0300

    update

--- app/meals/[mealslug]/page.js
+++ app/meals/[mealslug]/page.js
@@ -1,16 +1,41 @@
-import Link from 'next/link'
-import React from 'react'
+import Image from "next/image"
+import classes from "./page.module.css"
+import { getMeal } from "@/lib/meals"
+import { notFound } from "next/navigation";
 
-function MealDetails({params}) {
+export default  function MealDetails({params}) {
+  const slug = params?.mealslug;
+
+  if (!slug) {
+    return <p>Invalid meal slug.</p>;
+  }
+
+  const meal =  getMeal(slug);
+
+  if (!meal) {
+   notFound()
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br/>")
+
   return (
-    <main>
-        <h1>Meal Details</h1>
-        <p>Meal detal {params.mealslug}</p>
-        <p>
-            <Link href="/meals">Go to Meals</Link>
-        </p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image fill src={meal.image} alt={meal.title} />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p className={classes.instructions} dangerouslySetInnerHTML={{__html: meal.instructions}}></p>
+      </main>
+    </>
   )
 }
 
-export default MealDetails
